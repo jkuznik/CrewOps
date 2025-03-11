caa66dd0b69428d37c3aa843291f6e923c01a602
jkuznik <janusz.kuznik89@gmail.com>
2025-03-11 19:26:11 +0100

    Initial commit

---
+++ src/main/java/pl/kuznik/crewops/CrewOpsApplication.java
@@ -0,0 +1,13 @@
+package pl.kuznik.crewops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kuznik/crewops/CrewOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kuznik.crewops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
