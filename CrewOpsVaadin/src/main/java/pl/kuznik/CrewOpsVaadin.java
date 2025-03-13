d7034249a89f3c9df57d01486d691d934e3751b1
jkuznik <janusz.kuznik89@gmail.com>
2025-03-13 18:51:10 +0100

    CI pipeline test

--- CrewOpsVaadin/src/main/java/pl/kuznik/CrewOpsVaadin.java
+++ CrewOpsVaadin/src/main/java/pl/kuznik/CrewOpsVaadin.java
@@ -1,8 +1,5 @@
 package pl.kuznik;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CrewOpsVaadin {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
