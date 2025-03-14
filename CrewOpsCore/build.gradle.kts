6f9b4b4347369e38a6acdb25dc5caaea1c223f68
jkuznik <janusz.kuznik89@gmail.com>
2025-03-14 19:02:46 +0100

    Add necessary dependency and application config to achieve database connection

--- CrewOpsCore/build.gradle.kts
+++ CrewOpsCore/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql:42.7.5")
 }
 
