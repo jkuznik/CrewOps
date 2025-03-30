21ccf2be88c861893b326f753230cbaaf7aab82c
jkuznik <janusz.kuznik89@gmail.com>
2025-03-30 14:51:18 +0200

    Run spotless

--- CrewOpsCore/src/main/java/pl/kuznik/employee/EmployeeController.java
+++ CrewOpsCore/src/main/java/pl/kuznik/employee/EmployeeController.java
@@ -1,20 +1,17 @@
 package pl.kuznik.employee;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.kuznik.employee.dto.CreateEmployeeDTO;
 import pl.kuznik.entity.Employee;
 import pl.kuznik.entity.Qualification;
 import pl.kuznik.entity.Vehicle;
-
-import java.time.LocalDate;
-import java.util.Set;
 
 @RestController
 @RequestMapping("employees")
