5e6dd26e44ae15305c3251dcb89667501d322cf6
jkuznik <janusz.kuznik89@gmail.com>
2025-03-31 13:10:31 +0200

    Config shared testcontainers

---
+++ CrewOpsCore/src/test/java/pl/kuznik/IntegrationTest.java
@@ -0,0 +1,50 @@
+package pl.kuznik;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest(properties = "spring.profiles.active=integration")
+@Testcontainers
+public abstract class IntegrationTest {
+
+    public static final String TESTCONTAINER_DB_NAME = "testdb";
+    public static final String TESTCONTAINER_DB_USERNAME = "testUsername";
+    public static final String TESTCONTAINER_DB_PASSWORD = "testPassword";
+
+    @Container
+    public static final PostgreSQLContainer<?> postgresSQLContainer = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgis/postgis:16-3.4")
+                    .asCompatibleSubstituteFor("postgres"))
+            .withDatabaseName(TESTCONTAINER_DB_NAME)
+            .withUsername(TESTCONTAINER_DB_USERNAME)
+            .withPassword(TESTCONTAINER_DB_PASSWORD)
+            .withReuse(true);
+
+    @BeforeAll
+    public static void startContainer() {
+        postgresSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    public static void setTestContainerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgresSQLContainer::getPassword);
+    }
+
+    @Autowired
+    protected MockMvc mockMvc;
+}
