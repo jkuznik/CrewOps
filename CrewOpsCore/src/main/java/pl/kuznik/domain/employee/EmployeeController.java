45205b5404c75233429925f9f263b41bb3e665ca
jkuznik <janusz.kuznik89@gmail.com>
2025-04-02 15:50:57 +0200

    Centralize controllers URL values

--- CrewOpsCore/src/main/java/pl/kuznik/domain/employee/EmployeeController.java
+++ CrewOpsCore/src/main/java/pl/kuznik/domain/employee/EmployeeController.java
@@ -1,5 +1,7 @@
 package pl.kuznik.domain.employee;
 
+import static pl.kuznik.utils.enums.ControllerURL.*;
+
 import jakarta.validation.Valid;
 import java.time.Instant;
 import java.util.List;
@@ -11,29 +13,27 @@ import org.springframework.web.bind.annotation.*;
 import pl.kuznik.domain.employee.dto.CreateEmployeeDTO;
 import pl.kuznik.domain.employee.dto.EmployeeDTO;
 import pl.kuznik.domain.employee.dto.UpdateEmployeeDTO;
-import pl.kuznik.utils.enums.ControllerURL;
 
 @RestController
-@RequestMapping(ControllerURL.EMPLOYEES)
 @RequiredArgsConstructor
 class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @PostMapping()
+    @PostMapping(EMPLOYEES)
     public ResponseEntity<EmployeeDTO> createEmployee(@Valid @RequestBody CreateEmployeeDTO createEmployeeDTO) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(createEmployeeDTO));
     }
 
-    @GetMapping
+    @GetMapping(EMPLOYEES)
     public ResponseEntity<List<EmployeeDTO>> getEmployees() {
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.getAllEmployees());
     }
 
-    @PatchMapping("{employeeId}")
+    @PatchMapping(EMPLOYEES_EID)
     public ResponseEntity<EmployeeDTO> updateEmployee(
-            @PathVariable("employeeId") UUID employeeId,
+            @PathVariable(EMPLOYEE_ID) UUID employeeId,
             @RequestParam(required = false) String phoneNumber,
             @RequestParam(required = false) String department) {
         var updateEmployeeDTO = new UpdateEmployeeDTO(employeeId, phoneNumber, department);
@@ -41,16 +41,16 @@ class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.updateEmployee(updateEmployeeDTO));
     }
 
-    @PatchMapping("{employeeId}/qaulifications/{qualificationId}")
-    public ResponseEntity<EmployeeDTO> addEmployeeQaulification(
-            @PathVariable("employeeId") UUID employeeId, @PathVariable("qualificationId") UUID qualificationId) {
+    @PatchMapping(EMPLOYEES_EID_QUALIFICATIONS_QID)
+    public ResponseEntity<EmployeeDTO> addEmployeeQualification(
+            @PathVariable(EMPLOYEE_ID) UUID employeeId, @PathVariable(QUALIFICATION_ID) UUID qualificationId) {
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.addQualification(employeeId, qualificationId));
     }
 
-    @PatchMapping("{employeeId}/qaulifications/{qualificationId}/expire")
-    public ResponseEntity<EmployeeDTO> updateEmployeeQaulification(
-            @PathVariable("employeeId") UUID employeeId,
-            @PathVariable("qualificationId") UUID qualificationId,
+    @PatchMapping(EMPLOYEES_EID_QUALIFICATIONS_QID_EXPIRED)
+    public ResponseEntity<EmployeeDTO> updateEmployeeQualification(
+            @PathVariable(EMPLOYEE_ID) UUID employeeId,
+            @PathVariable(QUALIFICATION_ID) UUID qualificationId,
             @RequestBody Instant expireAt) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(employeeService.updateQualificationExpiredAt(employeeId, qualificationId, expireAt));
--- CrewOpsCore/src/main/java/pl/kuznik/domain/qualification/QualificationController.java
+++ CrewOpsCore/src/main/java/pl/kuznik/domain/qualification/QualificationController.java
@@ -1,5 +1,7 @@
 package pl.kuznik.domain.qualification;
 
+import static pl.kuznik.utils.enums.ControllerURL.*;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -8,21 +10,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kuznik.domain.qualification.dto.CreateQualificationDTO;
 import pl.kuznik.domain.qualification.dto.QualificationDTO;
-import pl.kuznik.utils.enums.ControllerURL;
 
 @RestController
-@RequestMapping(ControllerURL.QUALIFICATIONS)
 @RequiredArgsConstructor
 @Validated
 class QualificationController {
 
     private final QualificationService qualificationService;
 
-    @PostMapping("create")
+    @PostMapping(QUALIFICATIONS)
     public ResponseEntity<QualificationDTO> create(
             @RequestBody @Valid @NotNull CreateQualificationDTO createQualificationDTO) {
         return ResponseEntity.status(HttpStatus.OK)
