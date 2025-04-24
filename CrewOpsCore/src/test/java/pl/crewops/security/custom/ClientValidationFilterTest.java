7803dd238ce6429a26af95e53cd7c064d5d4f738
jkuznik <janusz.kuznik89@gmail.com>
2025-04-24 08:39:06 +0200

    ClientValidationFilter test

---
+++ CrewOpsCore/src/test/java/pl/crewops/security/custom/ClientValidationFilterTest.java
@@ -0,0 +1,66 @@
+package pl.crewops.security.custom;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import pl.crewops.security.config.PasswordEncoderConfig;
+import pl.crewops.security.config.SecurityConfigProperties;
+
+@SpringJUnitConfig(
+        classes = {
+            ClientValidationFilter.class,
+            SecurityConfigProperties.class,
+            PasswordEncoder.class,
+            AntPathRequestMatcher.class,
+            PasswordEncoderConfig.class
+        })
+class ClientValidationFilterTest {
+
+    @Autowired
+    private ClientValidationFilter clientValidationFilter;
+
+    @Autowired
+    private SecurityConfigProperties securityConfigProperties;
+
+    @MockitoBean
+    private AntPathRequestMatcher antPathRequestMatcher;
+
+    @MockitoBean
+    private PasswordEncoder passwordEncoder;
+
+    @MockitoBean
+    private HttpServletRequest request;
+
+    @MockitoBean
+    private HttpServletResponse response;
+
+    @MockitoBean
+    private FilterChain filterChain;
+
+    @Test
+    void doFilterInternal() throws ServletException, IOException {
+        // when
+        when(request.getRequestURI()).thenReturn("/test-endpoint");
+        when(request.getHeader("Client-Id")).thenReturn(securityConfigProperties.getClientIdInput());
+        when(passwordEncoder.matches(
+                        securityConfigProperties.getClientIdInput(), securityConfigProperties.getClientId()))
+                .thenReturn(true);
+
+        clientValidationFilter.doFilterInternal(request, response, filterChain);
+
+        // then
+        verify(filterChain, times(1)).doFilter(request, response);
+        verify(response, never()).sendError(anyInt(), anyString());
+    }
+}
