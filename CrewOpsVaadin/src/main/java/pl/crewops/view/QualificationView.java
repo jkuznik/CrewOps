0a79b4215d8b13ff005f45015b71a9a956438d1e
jkuznik <janusz.kuznik89@gmail.com>
2025-05-08 01:55:52 +0200

    Qualification already exist notification

--- CrewOpsVaadin/src/main/java/pl/crewops/view/QualificationView.java
+++ CrewOpsVaadin/src/main/java/pl/crewops/view/QualificationView.java
@@ -13,11 +13,13 @@ import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import java.util.ArrayList;
 import java.util.List;
 import pl.crewops.exceptions.NotAuthenticatedException;
 import pl.crewops.infrastructure.core.CoreAPI;
 import pl.crewops.security.jwt.JwtInfoService;
 import pl.crewops.view.component.mainLayout.MainLayout;
+import pl.crewops.view.component.notification.QualificationAlreadyExistNotification;
 import pl.crewops.view.form.QualificationForm;
 import pl.crewops.view.form.model.QualificationFormModel;
 
@@ -27,6 +29,8 @@ public class QualificationView extends MainLayout implements BeforeEnterObserver
     Grid<QualificationFormModel> grid = new Grid<>(QualificationFormModel.class);
     TextField filterText = new TextField();
     QualificationForm form;
+
+    List<QualificationFormModel> employees = new ArrayList<>();
 
     public QualificationView(CoreAPI coreAPI, JwtInfoService jwtInfoService) {
         super(coreAPI, jwtInfoService);
@@ -97,7 +101,7 @@ public class QualificationView extends MainLayout implements BeforeEnterObserver
 
     private void updateGrid() {
         try {
-            List<QualificationFormModel> employees = coreAPI.getAllQualifications().stream()
+            employees = coreAPI.getAllQualifications().stream()
                     .map(QualificationFormModel::toQualificationFormModel)
                     .toList();
 
@@ -123,8 +127,15 @@ public class QualificationView extends MainLayout implements BeforeEnterObserver
     }
 
     private void saveQualification(QualificationForm.SaveEvent event) {
+        if (employees.stream().anyMatch(qualification -> qualification
+                .getDescription()
+                .equals(event.getQualification().getDescription()))) {
+            new QualificationAlreadyExistNotification(event.getQualification().getDescription());
+            closeEditor();
+            return;
+        }
+
         try {
-            // TODO: add 'unique description' validation
             coreAPI.createQualification(QualificationFormModel.toCreateQualificationDTO(event.getQualification()));
             updateGrid();
             closeEditor();
@@ -175,9 +186,6 @@ public class QualificationView extends MainLayout implements BeforeEnterObserver
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         if (!jwtInfoService.validToken()) {
-            // TODO: try to show notification in this case
-            // TODO: add some listener of any action (add, edit, delete employee, etc.), currently navigation work only
-            // in case entering into secured view
             event.forwardTo(HomeView.class);
             UI.getCurrent().getPage().setLocation("/");
         }
---
+++ CrewOpsVaadin/src/main/java/pl/crewops/view/component/notification/QualificationAlreadyExistNotification.java
@@ -0,0 +1,22 @@
+package pl.crewops.view.component.notification;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+
+public class QualificationAlreadyExistNotification {
+    private final Notification notification = new Notification();
+
+    public QualificationAlreadyExistNotification(String description) {
+        notification.addClassName("update-vehicle-notification");
+        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+        notification.setPosition(Notification.Position.TOP_END);
+        notification.setDuration(5000);
+
+        Div div = new Div();
+        div.addClassName("update-vehicle-notification-div");
+        div.setText("Qualification '" + description + "' already exist");
+        notification.add(div);
+        notification.open();
+    }
+}
