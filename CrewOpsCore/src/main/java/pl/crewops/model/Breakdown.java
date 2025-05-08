583d645b3dfb1a5c45cacacaeb2bcc6fe521fb50
jkuznik <janusz.kuznik89@gmail.com>
2025-05-09 01:33:08 +0200

    Define breakdown

---
+++ CrewOpsCore/src/main/java/pl/crewops/model/Breakdown.java
@@ -0,0 +1,35 @@
+package pl.crewops.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "breakdown")
+public class Breakdown extends AbstractEntity {
+
+    @NotNull
+    private String description;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "reported_by_id")
+    private Employee reportedBy;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "repaired_by_id")
+    private Employee repairedBy;
+
+    private boolean solved;
+
+    private Instant solvedAt;
+}
