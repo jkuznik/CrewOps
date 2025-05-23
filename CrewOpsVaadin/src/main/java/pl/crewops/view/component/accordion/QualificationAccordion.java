27c1a319b7ab84bb051f5fefa8f7c5e85a609897
jkuznik <janusz.kuznik89@gmail.com>
2025-05-23 16:59:27 +0200

    Prepare qualification message

--- CrewOpsVaadin/src/main/java/pl/crewops/view/component/accordion/QualificationAccordion.java
+++ CrewOpsVaadin/src/main/java/pl/crewops/view/component/accordion/QualificationAccordion.java
@@ -9,7 +9,10 @@ import java.util.List;
 import java.util.Set;
 import pl.crewops.dto.qualification.QualificationDTO;
 
+// TODO: consider or just test if this component really have to extends FormLayout or some other
 public class QualificationAccordion extends FormLayout {
+
+    private final Accordion accordion = new Accordion();
 
     public QualificationAccordion() {
         addClassName("qualification-accordion");
@@ -17,19 +20,18 @@ public class QualificationAccordion extends FormLayout {
 
     public void getValues(Set<QualificationDTO> qualifications) {
         removeAll();
-        Accordion accordion = new Accordion();
         List<Span> items = new ArrayList<>();
 
         qualifications.forEach(qualification -> {
             items.add(new Span(qualification.description()));
         });
 
-        VerticalLayout qualificationDisplay = new VerticalLayout(items.toArray(new Span[items.size()]));
+        var qualificationDisplay = new VerticalLayout(items.toArray(new Span[items.size()]));
         qualificationDisplay.setSpacing(false);
         qualificationDisplay.setPadding(false);
 
         accordion.setVisible(true);
         add(accordion);
-        accordion.add("Qualifications", qualificationDisplay);
+        accordion.add(getTranslation("qualificationAccordion.title"), qualificationDisplay);
     }
 }
