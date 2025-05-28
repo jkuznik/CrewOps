a54f85a713f98ff583dff974ae2ec7c0877fb4b0
jkuznik <janusz.kuznik89@gmail.com>
2025-05-28 21:11:12 +0200

    Add TODOs

--- CrewOpsCore/src/main/java/pl/crewops/model/VehicleType.java
+++ CrewOpsCore/src/main/java/pl/crewops/model/VehicleType.java
@@ -13,6 +13,14 @@ import pl.crewops.dto.vehicleType.VehicleTypeDTO;
 @AllArgsConstructor
 public class VehicleType extends AbstractEntity {
 
+    // TODO:
+    // 1. Implement logic in case when there is no vehicle type for new vehicle (add new vehicle type)
+    // 2. Implement logic when there is vehicle type in db for new vehicle (select component with available types or
+    // etc.)
+    // 3. Implement logic to delete vehicle type when last vehicle of that type is removed from db
+    // 4. Implement CRUD for vehicle type domain
+    // 5. Test coverage for vehicle type domain
+
     @Column(nullable = false, unique = true)
     private String type;
 
