ec453485db0af9a0c1718600b9884ef749915ae0
jkuznik <janusz.kuznik89@gmail.com>
2025-06-01 19:41:39 +0200

    Multiple critical breakdown related to one vehicle not allow to set vehicle.broken = false

--- CrewOpsCore/src/main/java/pl/crewops/domain/breakdown/BreakdownRepository.java
+++ CrewOpsCore/src/main/java/pl/crewops/domain/breakdown/BreakdownRepository.java
@@ -1,9 +1,14 @@
 package pl.crewops.domain.breakdown;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.crewops.model.Breakdown;
+import pl.crewops.model.Vehicle;
 
 @Repository
-interface BreakdownRepository extends JpaRepository<Breakdown, UUID> {}
+interface BreakdownRepository extends JpaRepository<Breakdown, UUID> {
+
+    Optional<Breakdown> findFirstByVehicleAndSolvedIsFalse(Vehicle vehicle);
+}
