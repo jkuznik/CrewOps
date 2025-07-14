754dee1de102fdab98b5eac8d18f1a7b205202a3
jkuznik <janusz.kuznik89@gmail.com>
2025-07-15 01:49:17 +0200

    Correct version with hidden admin account inserts

--- CrewOpsCore/src/main/java/pl/crewops/utils/multitenancy/BootstrapDatabase.java
+++ CrewOpsCore/src/main/java/pl/crewops/utils/multitenancy/BootstrapDatabase.java
@@ -11,36 +11,33 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import pl.crewops.exception.multitenancy.CreateSchemaException;
 
 @Slf4j
 @Component
 @Profile(value = "dev")
-public class DevelopmentBootstrap {
+public class BootstrapDatabase {
 
     private final TenantSchemaInitializer tenantSchemaInitializer;
     private final LiquibaseSchemaMigrator liquibaseSchemaMigrator;
     private final DataSource dataSource;
+    private final Environment environment;
 
-    public DevelopmentBootstrap(
+    public BootstrapDatabase(
             TenantSchemaInitializer tenantSchemaInitializer,
             LiquibaseSchemaMigrator liquibaseSchemaMigrator,
-            DataSource dataSource) {
+            DataSource dataSource,
+            Environment environment) {
         this.tenantSchemaInitializer = tenantSchemaInitializer;
         this.liquibaseSchemaMigrator = liquibaseSchemaMigrator;
         this.dataSource = dataSource;
+        this.environment = environment;
 
         String testSchemaName = "testtenant_2f3b1d5c9e8f";
         String testValues = "db/changelog/insert/002-insert-development-requirement-values.yaml";
         executeInsert(tenantSchemaInitializer, liquibaseSchemaMigrator, dataSource, testSchemaName, testValues);
-
-        // TODO: modify way to create admin user before starts production
-        String adminSchemaName = "crewops_2b3b7d5c9e8f";
-        String adminValues = "db/changelog/insert/005-insert-admin-account-values.yaml";
-
-        //        executeInsert(tenantSchemaInitializer, liquibaseSchemaMigrator, dataSource, adminSchemaName,
-        // adminValues);
     }
 
     private static void executeInsert(
