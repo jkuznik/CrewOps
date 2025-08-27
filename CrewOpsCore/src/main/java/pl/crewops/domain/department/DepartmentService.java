2e905c8ce642349c8cf7f07cfbf87a1df64219d6
jkuznik <janusz.kuznik89@gmail.com>
2025-08-27 19:08:36 +0200

    TODO comment

--- CrewOpsCore/src/main/java/pl/crewops/domain/department/DepartmentService.java
+++ CrewOpsCore/src/main/java/pl/crewops/domain/department/DepartmentService.java
@@ -14,6 +14,7 @@ import pl.crewops.model.Department;
 @Service
 @RequiredArgsConstructor
 class DepartmentService implements DepartmentAPI {
+    // TODO: create CRUD for this domain and dedicated view/grid to maintain this on fe side
 
     private final DepartmentRepository departmentRepository;
 
