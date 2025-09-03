26f2f8c740535771a01f368f27f0514f94f60a92
jkuznik <janusz.kuznik89@gmail.com>
2025-09-03 19:15:33 +0200

    Fix mobile rendering for some components

--- CrewOpsVaadin/src/main/java/pl/crewops/infrastructure/health/HealthController.java
+++ CrewOpsVaadin/src/main/java/pl/crewops/infrastructure/health/HealthController.java
@@ -3,11 +3,12 @@ package pl.crewops.infrastructure.health;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.crewops.enums.ControllerURL;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("health")
+    @GetMapping(ControllerURL.HEALTH)
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
