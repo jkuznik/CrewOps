9f7f9984d7cbd3c85bdaf4fb5531ad875d6bccd3
jkuznik <janusz.kuznik89@gmail.com>
2025-09-06 02:51:30 +0200

    Update

--- CrewOpsVaadin/src/main/java/pl/crewops/config/RedisConfig.java
+++ CrewOpsVaadin/src/main/java/pl/crewops/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package pl.crewops.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+@Log4j2
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory lettuceConnectionFactoryDev() {
+        log.info("Current cache url: " + redisProperties.url());
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisProperties.url(), 6379));
     }
 }
