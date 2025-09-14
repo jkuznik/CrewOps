b1764fc0991f97e1daead05a5ca1496c10cf0989
jkuznik <janusz.kuznik89@gmail.com>
2025-09-14 23:56:11 +0200

    Update

--- CrewOpsVaadin/build.gradle.kts
+++ CrewOpsVaadin/build.gradle.kts
@@ -58,7 +58,8 @@ tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("boot
     environment.set(
         mapOf(
             "BP_JVM_VERSION" to "21",
-            "BP_CACHE_IMAGE" to "crewops-vaadin:latest"
+            "BP_CACHE_IMAGE" to "crewops-vaadin:latest",
+            "BP_JVM_CLASSPATH" to "app.jar"
         )
     )
 }
