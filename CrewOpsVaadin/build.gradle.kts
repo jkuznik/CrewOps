1d582f276ceeafd464fb7e8fbdbc250cbd4a17ec
Janusz Kuźnik <151467842+jkuznik@users.noreply.github.com>
2025-09-23 09:13:18 +0200

    Hotfix2 (#249)
    * hotfix 2
    
    * Update core workflow
    
    * Fix workflow config

--- CrewOpsVaadin/build.gradle.kts
+++ CrewOpsVaadin/build.gradle.kts
@@ -1,67 +1,67 @@
-plugins {
-        id("com.vaadin") version "24.6.6"
-    }
+    plugins {
+            id("com.vaadin") version "24.6.6"
+        }
 
-    repositories {
-        maven (url = uri("https://maven.vaadin.com/vaadin-addons"))
-    }
+        repositories {
+            maven (url = uri("https://maven.vaadin.com/vaadin-addons"))
+        }
 
-    extra["vaadinVersion"] = "24.6.6"
+        extra["vaadinVersion"] = "24.6.6"
 
-    val production: Boolean = project.hasProperty("production")
+        val production: Boolean = project.hasProperty("production")
 
-    dependencies {
+        dependencies {
 
-        if (production) {
-            // using vaadin-core instead of vaadin artifact should force to use only free components
-            implementation("com.vaadin:vaadin-core:24.7.1") {
-                exclude(group = "com.vaadin", module = "vaadin-dev")
+            if (production) {
+                // using vaadin-core instead of vaadin artifact should force to use only free components
+                implementation("com.vaadin:vaadin-core:24.7.1") {
+                    exclude(group = "com.vaadin", module = "vaadin-dev")
+                }
+            } else {
+                implementation("com.vaadin:vaadin-core:24.7.1")
             }
-        } else {
-            implementation("com.vaadin:vaadin-core:24.7.1")
-        }
-        implementation("com.vaadin:vaadin-spring-boot-starter")
-        implementation("org.springframework.boot:spring-boot-starter-cache")
-        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+            implementation("com.vaadin:vaadin-spring-boot-starter")
+            implementation("org.springframework.boot:spring-boot-starter-cache")
+            implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-        implementation(project(":CrewOpsModel"))
-        implementation(project(":CrewOpsSecurity"))
+            implementation(project(":CrewOpsModel"))
+            implementation(project(":CrewOpsSecurity"))
+        }
+
+        dependencyManagement {
+            imports {
+                mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+            }
+        }
+
+    vaadin {
+        optimizeBundle = true
+        pnpmEnable = true
     }
 
-    dependencyManagement {
-        imports {
-            mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
-        }
+    tasks.named("build") {
+        dependsOn("vaadinBuildFrontend")
     }
-
-vaadin {
-    optimizeBundle = true
-    pnpmEnable = true
-}
-
-tasks.named("build") {
-    dependsOn("vaadinBuildFrontend")
-}
-
-val imageTag: String = System.getenv("IMAGE_TAG") ?: "latest"
-
-tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
-    dependsOn("vaadinBuildFrontend")
 
     val imageTag: String = System.getenv("IMAGE_TAG") ?: "latest"
-    imageName.set("jkuznik-ecr/crewops-vaadin:$imageTag")
 
-    buildpacks.set(listOf(
-        "paketobuildpacks/amazon-corretto",
-        "paketobuildpacks/java"
-    ))
+    tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+        dependsOn("vaadinBuildFrontend")
 
-    environment.set(mapOf(
-        "BP_JVM_VERSION" to "21",
-        "SPRING_PROFILES_ACTIVE" to "production",
-        "SERVER_PORT" to "8081"
-    ))
-}
+        val imageTag: String = System.getenv("IMAGE_TAG") ?: "latest"
+        imageName.set("jkuznik-ecr/crewops-vaadin:$imageTag")
+
+        buildpacks.set(listOf(
+            "paketobuildpacks/amazon-corretto",
+            "paketobuildpacks/java"
+        ))
+
+        environment.set(mapOf(
+            "BP_JVM_VERSION" to "21",
+            "SPRING_PROFILES_ACTIVE" to "production",
+            "SERVER_PORT" to "8081"
+        ))
+    }
 
 
 
